4ed1aec917eb618929417960165abe67f0a339c8
Anh Dang <anhcdang@gmail.com>
2021-07-20 11:55:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let paragraph = document.querySelector('p#text')
+    paragraph.innerHTML = "This is really cool!"
+});
